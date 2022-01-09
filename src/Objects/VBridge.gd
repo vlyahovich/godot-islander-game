d587f20c74cc4799a381619427b5e76e705163c9
Vadim Liakhovich <v.lyahovic@gmail.com>
2022-01-09 18:08:31 +0100

    broken bridges

--- src/Objects/VBridge.gd
+++ src/Objects/VBridge.gd
@@ -1,15 +1,42 @@
 extends StaticBody2D
 
+export var broken = true setget set_broken
+
 const WORLD_BIT = 0
 const BRIDGES_BIT = 7
 
+func _ready():
+	_update_state()
+
+func set_broken(value):
+	broken = value
+
+	_update_state()
+
+func _update_state():
+	if !is_inside_tree():
+		return
+
+	if broken:
+		$TileMapBroken.visible = true
+		$TileMap.visible = false
+	else:
+		$TileMapBroken.visible = false
+		$TileMap.visible = true
+
 func _on_Area2D_body_entered(body):
+	if broken:
+		return
+
 	if "Player" in body.get_groups() or "Enemy" in body.get_groups():
 		body.set_collision_mask_bit(BRIDGES_BIT, true)
 		body.set_collision_mask_bit(WORLD_BIT, false)
 
 
 func _on_Area2D_body_exited(body):
+	if broken:
+		return
+
 	if "Player" in body.get_groups() or "Enemy" in body.get_groups():
 		body.set_collision_mask_bit(BRIDGES_BIT, false)
 		body.set_collision_mask_bit(WORLD_BIT, true)
