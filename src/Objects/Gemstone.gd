0f8711857d62455bea57feda86b37ab277294591
Vadim Liakhovich <v.lyahovic@gmail.com>
2022-01-09 22:57:00 +0100

    shining nova shader color modulation

--- src/Objects/Gemstone.gd
+++ src/Objects/Gemstone.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const gem_size = 3
+
 func _ready():
 	$CanvasLayer/AnimationPlayer.play("time")
 
@@ -7,6 +9,6 @@ func _physics_process(delta):
 	var material = $CanvasLayer/ColorRect.material as ShaderMaterial;
 	var transform = get_global_transform_with_canvas();
 	var viewport_size = $CanvasLayer/ColorRect.rect_size;
-	var center = transform.origin / viewport_size;
+	var center = ((transform.origin - Vector2(0, 3)) / viewport_size);
 
 	material.set_shader_param("center", Vector2(center.x, 1 - center.y))
